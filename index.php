44e3624d802b5efd7aada4dee7d0147a318e6bb8
SSD 5 <mojjaaaaa@gmail.com>
2024-01-09 12:06:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kartu Nama</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-slate-800">
+    <div class="judul w-full h-12 bg-gray-300 text-black text-2x2 font-bold flex justify-center items-center">
+        <h1>kartu</h1>
+
+    </div>
+    <div class="list-card mt-2 mx-6 flex flex-row flex-wrap item-strect justify-center items-center  gap-4 p-4 ">
+        <div class="card-name bg-slate-100 rounded-xl p-0 dark:bg-slate-700 flex flex-row">
+            <img src="asset/download.png" alt="" class="w-40 h-auto mx-auto rounded-xl">
+            <div class="p-8 text-left space-y-4">
+                <table class="table">
+                    <tr>
+                        <td class="font-bold text-base">Nama Siswa</td>
+                        <td class="font-semibold text-base">: kumar</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">NISN</td>
+                        <td class="font-semibold text-base">: 0123456789</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">Tingkatan</td>
+                        <td class="font-semibold text-base">: 12</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">Jurusan</td>
+                        <td class="font-semibold text-base">: RpL</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">Wali Kelas</td>
+                        <td class="font-semibold text-base">: Retno</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">Email</td>
+                        <td class="font-semibold text-base">: kumar@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">Nama ortu</td>
+                        <td class="font-semibold text-base">: rosep</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">telp ortu</td>
+                        <td class="font-semibold text-base">: 0832456765</td>
+                    </tr>
+                    <tr>
+                        <td class="font-bold text-base">kerjaan ortu</td>
+                        <td class="font-semibold text-base">: driver grab bike</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
